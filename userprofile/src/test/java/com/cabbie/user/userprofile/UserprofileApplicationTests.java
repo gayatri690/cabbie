4638b8bef247547a93020569b86d578a37a32a2b
Gayatri <survegayatri260@gmail.com>
2026-03-05 20:36:50 +0530

    userprofile microservice skeleton added

---
+++ userprofile/src/test/java/com/cabbie/user/userprofile/UserprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabbie.user.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
